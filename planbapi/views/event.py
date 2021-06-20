36122e33b5def6f3706d33578b3b642e78089810
bpotts11 <brittneympotts@gmail.com>
2021-06-20 14:14:01 -0500

    i broke it

--- planbapi/views/event.py
+++ planbapi/views/event.py
@@ -12,11 +12,33 @@ from planbapi.models import *
 class EventViewSet(ViewSet):
 
     def list(self, request):
-        events = Event.objects.all()
+        events = Event.objects.all().order_by('date')
+
+        current_user = self.request.query_params.get('user', None)
+        if current_user is not None:
+            events = events.filter(user=planb_customer)
 
         serializer = EventSerializer(
             events, many=True, context={'request': request})
         return Response(serializer.data)
+
+    # def list(self, request):
+    #     """Handle GET requests to events resource
+    #     Returns:
+    #         Response -- JSON serialized list of events
+    #     """
+    #     # Get the current authenticated user
+    #     planb_customer = Customer.objects.get(user=request.auth.user)
+    #     events = Event.objects.order_by('date')
+
+    #     # Support filtering posts by user
+    #     current_user = self.request.query_params.get('customer', None)
+    #     if current_user is not None:
+    #         events = events.filter(user=planb_customer)
+
+    #     serializer = EventSerializer(
+    #         events, many=True, context={'request': request})
+    #     return Response(serializer.data)
 
 
 class EventSerializer(serializers.ModelSerializer):
